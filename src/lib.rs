a8ceed4969a401b5e06064e6c85789e50f8cbc82
Carlos Barahona <carlos_nscl@hotmail.com>
2026-06-24 13:51:17 -0400

    user file igt to prevent accidental room splits
    instead of the level igt, this prevents unintended full game split locks

--- src/lib.rs
+++ src/lib.rs
@@ -244,10 +244,11 @@ async fn main() {
                                 }
                             }
 
+                            let last_room_transition_time = igt_file_secs_calculated.current - last_room_split_time;
                             if settings.splits_rooms
-                            && (igt_level_secs_calculated.current - last_room_split_time > 2.0 || mem_values.room_name.current != last_room_split_name)
+                            && (last_room_transition_time > 2.0 || last_room_transition_time < 0.0 || mem_values.room_name.current != last_room_split_name)
                             && mem_values.room_name.changed() && room_name_parsed_old != "rank_room" {
-                                last_room_split_time = igt_level_secs_calculated.current;
+                                last_room_split_time = igt_file_secs_calculated.current;
                                 last_room_split_name = mem_values.room_name.old;
 
                                 asr::timer::split();
